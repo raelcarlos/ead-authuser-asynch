a3ea14667593ea3c9f35245699aba64e6a10c6aa
Israel Carlos <israel.carlos@softplan.com.br>
2022-11-07 23:01:46 -0300

    Implementar Validation Tokens e Authorization com JWT

--- src/main/java/com/ead/authuser/configs/security/AuthenticationEntryPointImpl.java
+++ src/main/java/com/ead/authuser/configs/security/AuthenticationEntryPointImpl.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
         log.error("Unauthorized error: {}", e.getMessage());
         httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
